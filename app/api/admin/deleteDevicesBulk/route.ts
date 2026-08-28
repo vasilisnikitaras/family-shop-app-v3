de7306499326394fb3eaa588b178daaa20d7f473
vasilisnikitaras <vasilis.nikitaras@gmail.com>
2026-08-28 09:59:44 -0400

    Change DELETE query to use IN with UNNEST

--- app/api/admin/deleteDevicesBulk/route.ts
+++ app/api/admin/deleteDevicesBulk/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
 
     await sql`
       DELETE FROM devices
-      WHERE id = ANY(${ids}::int[]);
+      WHERE id IN (SELECT UNNEST(${ids}::int[]));
     `;
 
     return NextResponse.json({ success: true, deleted: ids.length });
